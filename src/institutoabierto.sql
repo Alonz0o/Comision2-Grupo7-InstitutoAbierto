020c8951a926caeb0f4d5d7fb40e62b388862443
Alonz0 <Alonz0@Alonz0o>
2018-12-12 01:27:17 -0300

    Agrege base de datos

---
+++ src/institutoabierto.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-12-2018 a las 05:24:21
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `institutoabierto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `idcursos` int(11) NOT NULL,
+  `idpersonas` int(11) NOT NULL,
+  `nombre` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `descripcion` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `costo` decimal(10,0) DEFAULT NULL,
+  `cupomaximo` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `habilitado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matriculas`
+--
+
+CREATE TABLE `matriculas` (
+  `idmatriculas` int(11) NOT NULL,
+  `idpersonas` int(11) NOT NULL,
+  `idcursos` int(11) NOT NULL,
+  `fechaalta` date NOT NULL,
+  `costo` decimal(10,0) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `idpersonas` int(11) NOT NULL,
+  `nombre` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `dni` varchar(9) CHARACTER SET utf8 NOT NULL,
+  `celular` varchar(15) CHARACTER SET utf8 NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `habilitado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`idcursos`),
+  ADD KEY `idpersonas` (`idpersonas`);
+
+--
+-- Indices de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  ADD PRIMARY KEY (`idmatriculas`),
+  ADD KEY `idcursos` (`idcursos`),
+  ADD KEY `idpersonas` (`idpersonas`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`idpersonas`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `idcursos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `idmatriculas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `idpersonas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`idpersonas`) REFERENCES `personas` (`idpersonas`);
+
+--
+-- Filtros para la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  ADD CONSTRAINT `matriculas_ibfk_1` FOREIGN KEY (`idcursos`) REFERENCES `cursos` (`idcursos`),
+  ADD CONSTRAINT `matriculas_ibfk_2` FOREIGN KEY (`idpersonas`) REFERENCES `personas` (`idpersonas`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
